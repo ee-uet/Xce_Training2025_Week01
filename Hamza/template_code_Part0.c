8d831ca2b02988be1f6a6f0f32f703c6a438666d
hamza-2274 <145056354+hamza-2274@users.noreply.github.com>
2025-09-08 12:01:27 +0500

    Update template_code_Part0.c

--- Hamza/template_code_Part0.c
+++ Hamza/template_code_Part0.c
@@ -4,101 +4,241 @@
 #include <time.h>
 #include <math.h>
 
-// ======================= Task 0.1 =======================
 void task01_datatypes() {
-    // TODO: Declare int, float, double, char
-    // Print their sizes and demonstrate type casting
+    int myint;
+    float myfloat;
+    double mydouble;
+    char mychar;
+
+    printf("int size: %zu\n", sizeof(myint));
+    printf("float size: %zu\n", sizeof(myfloat));
+    printf("double size: %zu\n", sizeof(mydouble));
+    printf("char size: %zu\n", sizeof(mychar));
+    
+    int a = 10;
+    float b = (float)a;
+    printf("casted value: %f\n", b);
 }
 
-// ======================= Task 0.2 =======================
 void task02_calculator() {
-    // TODO: Take two integers as input
-    // Perform arithmetic operations
-    // Implement switch-case calculator
+    int x, y;
+    char op;
+    printf("enter two integers and an operator: ");
+    scanf("%d %c %d", &x, &op, &y);
+
+    switch (op) {
+        case '+':
+            printf("result: %d\n", x + y);
+            break;
+        case '-':
+            printf("result: %d\n", x - y);
+            break;
+        case '*':
+            printf("result: %d\n", x * y);
+            break;
+        case '/':
+            printf("result: %f\n", (float)x / y);
+            break;
+        default:
+            printf("invalid operator\n");
+    }
 }
 
-// ======================= Task 0.3 =======================
 void task03_fibonacci() {
-    // TODO: Print Fibonacci sequence up to n terms
+    int n, i, a = 0, b = 1, next;
+    printf("enter number of terms: ");
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++) {
+        printf("%d ", a);
+        next = a + b;
+        a = b;
+        b = next;
+    }
+    printf("\n");
 }
 
 void task03_guessing_game() {
-    // TODO: Implement guessing game with random number
+    int number, guess;
+    number = rand() % 100 + 1;
+
+    do {
+        printf("guess a number (1-100): ");
+        scanf("%d", &guess);
+        if (guess > number) {
+            printf("too high\n");
+        } else if (guess < number) {
+            printf("too low\n");
+        }
+    } while (guess != number);
+
+    printf("you got it!\n");
 }
 
-// ======================= Task 0.4 =======================
 int isPrime(int n) {
-    // TODO: Return 1 if n is prime, else 0
-    return 0;
-}
-
-void task04_prime_numbers() {
-    // TODO: Print prime numbers between 1 and 100
-}
-
-int factorial(int n) {
-    // TODO: Implement recursive factorial function
+    if (n <= 1) return 0;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) return 0;
+    }
     return 1;
 }
 
-// ======================= Task 0.5 =======================
+void task04_prime_numbers() {
+    printf("primes between 1 and 100:\n");
+    for (int i = 1; i <= 100; i++) {
+        if (isPrime(i)) {
+            printf("%d ", i);
+        }
+    }
+    printf("\n");
+}
+
+int factorial(int n) {
+    if (n == 0) return 1;
+    return n * factorial(n - 1);
+}
+
 void task05_reverse_string() {
-    // TODO: Reverse a string without library functions
+    char str[100];
+    int len, i;
+    printf("enter a string: ");
+    scanf("%s", str);
+    len = strlen(str);
+
+    for (i = 0; i < len / 2; i++) {
+        char temp = str[i];
+        str[i] = str[len - i - 1];
+        str[len - i - 1] = temp;
+    }
+    printf("reversed string: %s\n", str);
 }
 
 void task05_second_largest() {
-    // TODO: Find the second largest element in an array
+    int arr[] = {12, 35, 1, 10, 34, 1};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int first, second;
+
+    first = second = -2147483648;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > first) {
+            second = first;
+            first = arr[i];
+        } else if (arr[i] > second && arr[i] != first) {
+            second = arr[i];
+        }
+    }
+
+    if (second == -2147483648) {
+        printf("no second largest element\n");
+    } else {
+        printf("second largest: %d\n", second);
+    }
 }
 
-// ======================= Task 0.6 =======================
 void task06_file_io() {
-    // TODO: Write 5 integers to a file, then read them back
+    FILE *fp;
+    int nums[5] = {10, 20, 30, 40, 50};
+    int read_nums[5];
+
+    fp = fopen("numbers.txt", "w");
+    for (int i = 0; i < 5; i++) {
+        fprintf(fp, "%d\n", nums[i]);
+    }
+    fclose(fp);
+
+    fp = fopen("numbers.txt", "r");
+    for (int i = 0; i < 5; i++) {
+        fscanf(fp, "%d", &read_nums[i]);
+    }
+    fclose(fp);
+
+    for (int i = 0; i < 5; i++) {
+        printf("read: %d\n", read_nums[i]);
+    }
 }
 
-// ======================= Task 0.7 =======================
 void task07_bitwise_ops() {
-    // TODO: Demonstrate AND, OR, XOR, NOT, shifts
-    // Bonus: Check if number is power of 2
+    int x = 5, y = 3;
+    printf("and: %d\n", x & y);
+    printf("or: %d\n", x | y);
+    printf("xor: %d\n", x ^ y);
+    printf("not: %d\n", ~x);
+    printf("left shift: %d\n", x << 1);
+    printf("right shift: %d\n", x >> 1);
+
+    int n = 8;
+    if ((n & (n - 1)) == 0) {
+        printf("%d is a power of 2\n", n);
+    } else {
+        printf("%d is not a power of 2\n", n);
+    }
 }
 
-// ======================= Task 0.8 =======================
+typedef enum { sun, mon, tue, wed, thu, fri, sat } weekday;
 void task08_enum_weekday() {
-    // TODO: Map number (1–7) to day of week using enum
+    int day_num;
+    printf("enter day number (1-7): ");
+    scanf("%d", &day_num);
+    
+    weekday today = day_num - 1;
+
+    switch (today) {
+        case sun: printf("sunday\n"); break;
+        case mon: printf("monday\n"); break;
+        case tue: printf("tuesday\n"); break;
+        case wed: printf("wednesday\n"); break;
+        case thu: printf("thursday\n"); break;
+        case fri: printf("friday\n"); break;
+        case sat: printf("saturday\n"); break;
+        default: printf("invalid day\n"); break;
+    }
 }
 
-// ======================= Task 0.9 =======================
 struct Point {
     int x;
     int y;
 };
 
 void task09_struct_distance() {
-    // TODO: Take two points and calculate Euclidean distance
+    struct Point p1, p2;
+    double distance;
+    printf("enter point 1 (x y): ");
+    scanf("%d %d", &p1.x, &p1.y);
+    printf("enter point 2 (x y): ");
+    scanf("%d %d", &p2.x, &p2.y);
+
+    distance = sqrt(pow(p2.x - p1.x, 2) + pow(p2.y - p1.y, 2));
+    printf("distance: %f\n", distance);
 }
 
-// ======================= Task 0.10 =======================
 void task10_cmd_args(int argc, char *argv[]) {
-    // TODO: Take 2 integers as command line args and print sum
+    if (argc != 3) {
+        printf("usage: %s <int1> <int2>\n", argv[0]);
+        return;
+    }
+    
+    int num1 = atoi(argv[1]);
+    int num2 = atoi(argv[2]);
+    printf("sum: %d\n", num1 + num2);
 }
 
-// ======================= Main =======================
 int main(int argc, char *argv[]) {
-    srand(time(NULL)); // Seed random numbers
+    srand(time(NULL));
 
-    // Uncomment tasks as you implement them
-    // task01_datatypes();
-    // task02_calculator();
-    // task03_fibonacci();
-    // task03_guessing_game();
-    // task04_prime_numbers();
-    // printf("Factorial of 5 = %d\n", factorial(5));
-    // task05_reverse_string();
-    // task05_second_largest();
-    // task06_file_io();
-    // task07_bitwise_ops();
-    // task08_enum_weekday();
-    // task09_struct_distance();
-    // task10_cmd_args(argc, argv);
+    task01_datatypes();
+    task02_calculator();
+    task03_fibonacci();
+    task03_guessing_game();
+    task04_prime_numbers();
+    printf("factorial of 5 = %d\n", factorial(5));
+    task05_reverse_string();
+    task05_second_largest();
+    task06_file_io();
+    task07_bitwise_ops();
+    task08_enum_weekday();
+    task09_struct_distance();
+    task10_cmd_args(argc, argv);
 
     return 0;
 }
